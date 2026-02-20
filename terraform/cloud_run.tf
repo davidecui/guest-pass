d2409cbd5186406a93cba086e1f9c24e04cba3b4
Davide Cui <davide.cui@gmail.com>
2026-02-20 22:26:00 +0100

    feat: add Cloud Run service definition for the guest-pass application.

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.region
-  ingress = "INGRESS_TRAFFIC_ALL"
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     containers {
